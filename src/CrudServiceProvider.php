c62c4240c7f5758ef185274fadde6180b76724a4
Ashif <ashiful134@gmail.com>
2021-12-25 22:51:23 +0600

    update

--- src/CrudServiceProvider.php
+++ src/CrudServiceProvider.php
@@ -25,11 +25,18 @@ class CrudServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__ . '/config/crud.php' => config_path('crud.php'),
-            __DIR__ . '/stubs/views/create.stub' => resource_path('views/vendor/crud/create.stub'),
-            __DIR__ . '/stubs/views/edit.stub' => resource_path('views/vendor/crud/edit.stub'),
-            __DIR__ . '/stubs/views/form-field.stub' => resource_path('views/vendor/crud/form-field.stub'),
-            __DIR__ . '/stubs/views/form.stub' => resource_path('views/vendor/crud/form.stub'),
-            __DIR__ . '/stubs/views/form.stub' => resource_path('views/vendor/crud/form.stub'),
+            __DIR__ . '/stubs/views/create.stub' => base_path('stubs/crud/views/create.stub'),
+            __DIR__ . '/stubs/views/edit.stub' => base_path('stubs/crud/views/edit.stub'),
+            __DIR__ . '/stubs/views/form-field.stub' => base_path('stubs/crud/views/form-field.stub'),
+            __DIR__ . '/stubs/views/form.stub' => base_path('stubs/crud/views/form.stub'),
+            __DIR__ . '/stubs/views/index.stub' => base_path('stubs/crud/views/index.stub'),
+            __DIR__ . '/stubs/views/show.stub' => base_path('stubs/crud/views/show.stub'),
+            __DIR__ . '/stubs/views/view-field.stub' => base_path('stubs/crud/views/view-field.stub'),
+            __DIR__ . '/stubs/Controller.stub' => base_path('stubs/crud/Controller.stub'),
+            __DIR__ . '/stubs/Migration.stub' => base_path('stubs/crud/Migration.stub'),
+            __DIR__ . '/stubs/Model.stub' => base_path('stubs/crud/Model.stub'),
+            __DIR__ . '/stubs/Repository.stub' => base_path('stubs/crud/Repository.stub'),
+            __DIR__ . '/stubs/Request.stub' => base_path('stubs/crud/Request.stub'),
         ], 'crud');
     }
 
